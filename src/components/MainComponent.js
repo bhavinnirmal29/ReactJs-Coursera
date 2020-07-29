9aa32994dde827ae2fcc804f04eeb52fbbf9f931
Bhavin Nirmal <bhavinnirmal29@gmail.com>
2020-07-29 23:15:26 +0530

    Incomplete SPA

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,8 +3,12 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import {BrowserRouter,Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
@@ -12,7 +16,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
     };
   }
 
@@ -23,21 +29,24 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return (
-        <Home />
+        <Home 
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
     return (
       <div>
         <Header />
-        <BrowserRouter>
           <Switch>
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+            <Route exact path='/contactus' component={Contact} />} />
             <Redirect to="/home" />
           </Switch>
-        </BrowserRouter>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.dishes.selectedDish)[0]} />
         <Footer />
       </div>
     );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Card,CardImg,CardImgOverlay,CardTitle} from 'reactstrap';
 
-  function RenderMenuItem({dish,onClick}){
+  function RenderMenuItem({ dish, onClick }){
     return(
       <Card key={dish.id} onClick={() => onClick(dish.id)}>
         <CardImg width="100%" src={dish.image} alt={dish.name} />
