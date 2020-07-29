a95b302c6a9dc0680f2c79f65b1d0c04fca3278b
Bhavin Nirmal <bhavinnirmal29@gmail.com>
2020-07-29 17:19:02 +0530

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React,{Component} from 'react';
 // import logo from './logo.svg';
-import {Navbar,NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
 import './App.css';
 import {DISHES} from './shared/dishes';
-
+import Main from './components/MainComponent';
 class App extends Component {
   constructor(props){
     super(props);
@@ -15,14 +13,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">  
-              Ristorante Con Fusion
-            </NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes = {this.state.dishes}/>
+        <Main />
       </div>
     );
   }
